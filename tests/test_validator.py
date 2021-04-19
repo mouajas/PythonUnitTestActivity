f2c6f223698b76f5a931bdd6c4668556e2efe78c
Jasmine Moua <jmoua@csumb.edu>
2021-04-19 03:30:22 +0000

    Wrote test for special characters

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -47,3 +47,13 @@ class TestValidator(unittest.TestCase):
 
         # Assert
         self.assertTrue(result)
+
+    def test_it_will_reject_username_with_special_characters(self):
+        # Assume
+        username = 'Reval$'
+        
+        # Action
+        result = self.validator.username_is_valid(username)
+        
+        # Assert
+        self.assertFalse(result)
